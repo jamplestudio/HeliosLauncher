75839c136c227cf951023d739bc65559eeded030
Uni0305 <git@uni0305.me>
2024-08-01 13:30:08 +0900

    Refactor: Remove call to initNews() in onDistroRefresh()

--- app/assets/js/scripts/uibinder.js
+++ app/assets/js/scripts/uibinder.js
@@ -131,7 +131,6 @@ function showFatalStartupError(){
 function onDistroRefresh(data){
     updateSelectedServer(data.getServerById(ConfigManager.getSelectedServer()))
     refreshServerStatus()
-    initNews()
     syncModConfigurations(data)
     ensureJavaSettings(data)
 }
